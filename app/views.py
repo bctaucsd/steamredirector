938aa63752358b0731783a5b22da00708004e738
bctaucsd <brian.ta626@gmail.com>
2016-05-14 23:06:24 -0700

    initial push

---
+++ app/views.py
@@ -0,0 +1,43 @@
+from flask import render_template, redirect
+from app import app
+
+# square = 5
+@app.route('/')
+@app.route('/index')
+def index():
+    print "hello world"
+    user = {'nickname': 'Miguel'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
+@app.route('/<game_name>/<int:square>')
+def index2(square,game_name):
+    print "----------------------------------------"
+    print square
+    print "----------------------------------------"
+    return render_template("button.html", square=square,game_name=game_name)
+@app.route('/<int:square>')
+def index3(square):
+    print "----------------------------------------"
+    print square
+    print "----------------------------------------"
+    return render_template("button_alt.html", square=square)
+@app.route('/<game_name>/<int:square>/<image_url>')
+def index4(square,game_name,image_url):
+    print "----------------------------------------"
+    print square
+    print "----------------------------------------"
+    return render_template("button_alternate.html", square=square,game_name=game_name,image_url=image_url)
+
+    # return redirect('steam://store/'+str(square),302)
